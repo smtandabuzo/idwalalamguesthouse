f8e36bd83d7f644c38ae14b9c6b5fb3b7eb96cc7
Sazi Mtandabuzo <Sazi Mtandabuzo>
2017-06-03 16:07:34 +0200

    Make more WAVE modifications to attractions and services files

---
+++ search.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>search result</title>
+<meta charset="utf-8">    
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="img/favicon.ico" type="image/x-icon">
+<link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon" />
+<meta name="description" content="Your description">
+<meta name="keywords" content="Your keywords">
+<meta name="author" content="Your name">
+<!--CSS-->
+<link rel="stylesheet" href="css/bootstrap.css" >
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="fonts/font-awesome.css">
+<!--JS-->
+<script src="js/jquery.js"></script>
+<script src="js/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/superfish.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+<script src="search/search.js"></script>
+<script src="js/jquery.mobilemenu.js"></script> 
+<script src="js/jquery.ui.totop.js"></script>
+<script src="js/jquery.equalheights.js"></script> 
+<!--[if lt IE 9]>
+    <div style='text-align:center' class="qwerty"><a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div>  
+  <![endif]-->
+  <!--[if lt IE 9]><script src="../docs-assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+  <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+  <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+  <![endif]-->
+</head>
+<body>
+<!--header-->
+<header>
+    <div class="container"> 
+        <h1 class="navbar-brand navbar-brand_"><a href="index.html"><img src="img/logo.png" alt="" class="logo_large"><img src="img/logo_medium.png" alt="" class="logo_medium"><span>company name</span></a></h1>
+    </div>
+    <div class="follow-box">
+        <div class="container"> 
+            <ul class="follow_icon">
+                <li><a href="#"><img src="img/follow_icon1.png" alt=""></a></li>
+                <li><a href="#"><img src="img/follow_icon2.png" alt=""></a></li>
+                <li><a href="#"><img src="img/follow_icon3.png" alt=""></a></li>
+                <li><a href="#"><img src="img/follow_icon4.png" alt=""></a></li>
+                <li><a href="#"><img src="img/follow_icon5.png" alt=""></a></li>
+            </ul>
+            <ul class="address_icon">
+                <li><img src="img/page1_icon1.png" alt=""> 9870 St Vincent Place, Glasgow, DC 45 Fr 45.</li>
+                <li><img src="img/page1_icon2.png" alt=""> <a href="#">contact@demolink.org</a></li>
+                <li><img src="img/page1_icon3.png" alt=""> +1 800 559 6580</li>
+            </ul>
+        </div>
+    </div>
+</header>
+<div class="menuBox">  
+    <div class="container"> 
+        <nav class="navbar navbar-default navbar-static-top tm_navbar clearfix" role="navigation">
+            <ul class="nav sf-menu clearfix">
+                <li><a href="index.html">Home</a></li>
+                <li class="sub-menu"><a href="index-1.html">About</a><span></span>
+                    <ul class="submenu">
+        				<li><a href="#">history</a></li>
+        				<li><a href="#">news</a><span></span>
+                            <ul class="submenu">
+                                <li><a href="#">latest</a></li>
+                                <li><a href="#">archive</a></li>
+                              </ul> 
+                        </li>
+        				<li><a href="#">testimonials</a></li>
+        			</ul>
+                </li>
+                <li><a href="index-2.html">Services</a></li>
+                <li><a href="index-3.html">Gallery</a></li>
+                <li><a href="index-4.html">Contacts</a></li>
+            </ul>
+        </nav>
+    </div>
+</div>
+<div class="bg_pic"></div>
+<div class="global indent">
+    <!--content-->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12">
+                <h2>Search result:</h2>
+                <div id="search-results"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<!--footer-->
+<footer>
+    <div class="container">
+            <div class="row">
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+                    <div class="box maxheight2">
+                        <p class="title">NikHotel</p>
+                        <p><span>copyright</span> &copy; <em id="copyright-year"></em> <a href="index-5.html">Privacy policy</a></p>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+                    <div class="box maxheight2">
+                        <p class="title">E-Mail Us</p>
+                        <a href="#" class="lnk">mail@demolink.org</a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+                    <div class="box maxheight2">
+                        <p class="title">Follow Us</p>
+                        <ul class="follow_icon2">
+                            <li><a href="#"><img src="img/follow_icon6.png" alt=""></a></li>
+                            <li><a href="#"><img src="img/follow_icon7.png" alt=""></a></li>
+                            <li><a href="#"><img src="img/follow_icon8.png" alt=""></a></li>
+                            <li><a href="#"><img src="img/follow_icon9.png" alt=""></a></li>
+                            <li><a href="#"><img src="img/follow_icon10.png" alt=""></a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-3 col-sm-6 col-xs-6">
+                    <div class="box indent maxheight2">
+                        <p class="title">Address</p>
+                        <p class="txt">9870 St Vincent Place,<br>Glasgow, DC 45 Fr 45.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+</footer>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/tm-scripts.js"></script>
+</body>
+</html>
